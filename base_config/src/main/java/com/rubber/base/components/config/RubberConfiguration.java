715fa17f31f29850cb5b433de4bfa141b44042b8
luffyu <919132691@qq.com>
2021-01-09 23:03:05 +0800

    修改自定义的配置

--- base_config/src/main/java/com/rubber/base/components/config/RubberConfiguration.java
+++ base_config/src/main/java/com/rubber/base/components/config/RubberConfiguration.java
@@ -1,26 +1,18 @@
 package com.rubber.base.components.config;
 
+import com.alibaba.cloud.nacos.NacosConfigManager;
 import lombok.Data;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /**
  * @author luffyu
  * Created on 2020/12/13
  */
-@Configuration
 @Data
-@ConfigurationProperties(prefix = "rubber.components")
 public class RubberConfiguration {
 
-
-    private ComponentsConfigProperties config;
-
-
-
     @Bean
-    public RubberEnvironmentPostProcessor myPropertySourceLocator(){
-        return new RubberEnvironmentPostProcessor(config);
+    public RubberPropertySourceLocator myPropertySourceLocator(NacosConfigManager nacosConfigManager){
+        return new RubberPropertySourceLocator(nacosConfigManager);
     }
 }
--- base_config/src/main/java/com/rubber/base/components/config/RubberPropertySourceLocator.java
+++ base_config/src/main/java/com/rubber/base/components/config/RubberPropertySourceLocator.java
@@ -1,48 +1,43 @@
 package com.rubber.base.components.config;
 
 import cn.hutool.core.map.MapUtil;
-import cn.hutool.core.util.StrUtil;
+import com.alibaba.cloud.nacos.NacosConfigManager;
+import com.alibaba.cloud.nacos.NacosConfigProperties;
 import com.alibaba.cloud.nacos.parser.NacosDataParserHandler;
-import com.alibaba.nacos.api.NacosFactory;
-import com.alibaba.nacos.api.config.ConfigService;
-import com.rubber.base.components.config.exception.RubberConfigException;
+import lombok.Data;
 import org.springframework.cloud.bootstrap.config.PropertySourceLocator;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.MapPropertySource;
 import org.springframework.core.env.PropertySource;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author luffyu
  * Created on 2020/12/13
  */
 @Order(1)
-public class RubberEnvironmentPostProcessor implements PropertySourceLocator {
+@Data
+public class RubberPropertySourceLocator implements PropertySourceLocator {
 
 
+    private NacosConfigProperties nacosConfigProperties;
 
-    public ComponentsConfigProperties componentsConfigProperties;
+    private NacosConfigManager nacosConfigManager;
 
 
-    public RubberEnvironmentPostProcessor(ComponentsConfigProperties componentsConfigProperties) {
-        this.componentsConfigProperties = componentsConfigProperties;
+    public RubberPropertySourceLocator(NacosConfigManager nacosConfigManager) {
+        this.nacosConfigManager = nacosConfigManager;
+        this.nacosConfigProperties = nacosConfigManager.getNacosConfigProperties();
     }
-
 
 
     @Override
     public PropertySource<?> locate(Environment environment) {
         try {
-            String nacosServerAdd = "127.0.0.1:8848";
-            if (StrUtil.isEmpty(nacosServerAdd)){
-                throw new RubberConfigException("spring.cloud.nacos.server-addr is null");
-            }
-            Properties properties = new Properties();
-            properties.put("serverAddr", nacosServerAdd);
-            ConfigService configService = NacosFactory.createConfigService(properties);
-            Map<String, Object> p = initConfigData(configService,"dev",environment);
+            Map<String, Object> p = initConfigData("dev",environment);
             return new MapPropertySource("DEFAULT_GROUP,test.yml", p);
         }catch (Exception e){
             throw new RuntimeException();
@@ -51,7 +46,7 @@ public class RubberEnvironmentPostProcessor implements PropertySourceLocator {
 
 
 
-    private Map<String,Object> initConfigData(ConfigService configService,String projectActive,Environment environment){
+    private Map<String,Object> initConfigData(String projectActive,Environment environment){
         Map<String,String> dataIdMap = createDataId(projectActive,environment);
         if (MapUtil.isEmpty(dataIdMap)){
             return null;
@@ -61,7 +56,7 @@ public class RubberEnvironmentPostProcessor implements PropertySourceLocator {
         for (Map.Entry<String,String> dataId:dataIdMap.entrySet()){
 
             try {
-                String value = configService.getConfig(dataId.getKey(), dataId.getValue(), 5000);
+                String value = nacosConfigManager.getConfigService().getConfig(dataId.getKey(), dataId.getValue(), 5000);
                 Map<String, Object> p = NacosDataParserHandler.getInstance()
                         .parseNacosData(value, "yml");
                 result.putAll(p);
@@ -79,7 +74,6 @@ public class RubberEnvironmentPostProcessor implements PropertySourceLocator {
     private Map<String,String> createDataId(String projectActive,Environment environment){
         String dataIdPrefix = "rubber-config-";
         String group = "DEFAULT_GROUP";
-        String dbSet = environment.getProperty("rubber.components.config.dbSet");
         Map<String,String> dataIds = new HashMap<>(10);
         String dbDataId = dataIdPrefix + "mysql-" + projectActive + "-userDb.yml";
         dataIds.putIfAbsent(dbDataId,group);
