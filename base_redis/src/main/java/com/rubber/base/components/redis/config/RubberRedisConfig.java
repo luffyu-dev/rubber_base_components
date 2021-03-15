a11e54b22981be4afce64bf036eb83965a76021a
luffyu <919132691@qq.com>
2021-03-15 23:42:14 +0800

    新增日志框架

--- base_redis/src/main/java/com/rubber/base/components/redis/config/RubberRedisConfig.java
+++ base_redis/src/main/java/com/rubber/base/components/redis/config/RubberRedisConfig.java
@@ -1,17 +1,12 @@
 package com.rubber.base.components.redis.config;
 
-import com.rubber.base.components.redis.RubberJedisClusterProxy;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import redis.clients.jedis.HostAndPort;
 
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * <p>redis配置文件</p>
@@ -31,29 +26,29 @@ public class RubberRedisConfig extends CachingConfigurerSupport {
 
 
 
-    @Bean
-    public RubberJedisClusterProxy getJedisCluster() {
-        String redisSetName = "normalRedis";
-        RedisClusterConfigProperties setClusterProperties = cluster.get(redisSetName);
-        if (!RedisClusterType.CLUSTER.equals(setClusterProperties.getRedisClusterType())){
-            return null;
-        }
-        // 截取集群节点
-        String[] cluster = setClusterProperties.getNodes().toArray(new String[0]);
-        // 创建set集合
-        Set<HostAndPort> nodes = new HashSet<>();
-        // 循环数组把集群节点添加到set集合中
-        for (String node : cluster) {
-            String[] host = node.split(":");
-            //添加集群节点
-            nodes.add(new HostAndPort(host[0], Integer.parseInt(host[1])));
-        }
-        return new RubberJedisClusterProxy(nodes,
-                setClusterProperties.getConnectionTimeout(),
-                setClusterProperties.getSoTimeout(),
-                setClusterProperties.getMaxAttempts(),
-                setClusterProperties.getPassword(), setClusterProperties.getJedisPoolConfig());
-    }
+//    @Bean
+//    public RubberJedisClusterProxy getJedisCluster() {
+//        String redisSetName = "normalRedis";
+//        RedisClusterConfigProperties setClusterProperties = cluster.get(redisSetName);
+//        if (!RedisClusterType.CLUSTER.equals(setClusterProperties.getRedisClusterType())){
+//            return null;
+//        }
+//        // 截取集群节点
+//        String[] cluster = setClusterProperties.getNodes().toArray(new String[0]);
+//        // 创建set集合
+//        Set<HostAndPort> nodes = new HashSet<>();
+//        // 循环数组把集群节点添加到set集合中
+//        for (String node : cluster) {
+//            String[] host = node.split(":");
+//            //添加集群节点
+//            nodes.add(new HostAndPort(host[0], Integer.parseInt(host[1])));
+//        }
+//        return new RubberJedisClusterProxy(nodes,
+//                setClusterProperties.getConnectionTimeout(),
+//                setClusterProperties.getSoTimeout(),
+//                setClusterProperties.getMaxAttempts(),
+//                setClusterProperties.getPassword(), setClusterProperties.getJedisPoolConfig());
+//    }
 
 
 
