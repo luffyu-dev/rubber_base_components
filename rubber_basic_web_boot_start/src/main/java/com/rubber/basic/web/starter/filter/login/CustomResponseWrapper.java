cd0d43471ee0119fb5aad009b9cfe79f0765be69
rock <rock233at@gmail.com>
2025-03-08 14:11:42 +0800

    解决表情异常的问题

--- rubber_basic_web_boot_start/src/main/java/com/rubber/basic/web/starter/filter/login/CustomResponseWrapper.java
+++ rubber_basic_web_boot_start/src/main/java/com/rubber/basic/web/starter/filter/login/CustomResponseWrapper.java
@@ -2,6 +2,7 @@ package com.rubber.basic.web.starter.filter.login;
 
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 import java.io.IOException;
@@ -12,9 +13,11 @@ public class CustomResponseWrapper extends ContentCachingResponseWrapper {
         super(response);
     }
 
+
+
     public String getCapturedResponse() throws IOException {
         flushBuffer(); // 确保所有数据都写入缓存
-        return new String(getContentAsByteArray(), getCharacterEncoding()); // 读取缓存数据
+        return new String(getContentAsByteArray(), StandardCharsets.UTF_8); // 读取缓存数据
     }
 
     public void writeModifiedResponse(String modifiedContent) throws IOException {
